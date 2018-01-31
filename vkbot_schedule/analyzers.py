049d5d0bc5c1d595b551bdd8e12099e1fea02f43
Firexd2 <firedenis@yandex.ru>
2018-01-31 18:31:59 +0300

    Add filters

--- vkbot_schedule/analyzers.py
+++ vkbot_schedule/analyzers.py
@@ -1,4 +1,5 @@
 from datetime import *
+from vkbot_schedule.filters import *
 from vkbot_schedule.models import *
 
 
@@ -9,10 +10,15 @@ def command_analyzer(query, uid):
     q = list_query[1:]
 
     dict_command = {'!каждыйдень': query_analyzer_every_day,
+                    '!кд': query_analyzer_every_day,
                     '!каждуюнеделю': query_analyzer_every_week,
+                    '!кн': query_analyzer_every_week,
                     '!каждыймесяц': query_analyzer_every_month,
+                    '!км': query_analyzer_every_month,
                     '!каждыйгод': query_analyzer_every_year,
-                    '!день': query_analyzer_day}
+                    '!кг': query_analyzer_every_year,
+                    '!день': query_analyzer_day,
+                    '!д': query_analyzer_day}
 
     try:
         response = dict_command[command](q, uid)
@@ -26,7 +32,7 @@ def command_analyzer(query, uid):
 
 def actions_analyzer(query, uid):
 
-    list_query = query.split(' ') # сохраняеи запрос в виде списка через пробел
+    list_query = query.split(' ') # сохраняем запрос в виде списка через пробел
     action = list_query[0].lower()
     q = list_query[1:]
 
@@ -45,10 +51,12 @@ def query_analyzer_every_day(query, uid):
     """
     Тест 10:00,13:00,15:00 Это-сообщение-прошу-повторить-мне
     """
+    response_filter = filter_every_day(query)
+    if response_filter:
+        return response_filter
 
     times = query[1].split(',') # разбиваем времена через запятую
     instance_times = [] # необходимые экземпляры для сохранения
-
     message = (' ').join(query[2].split('-')) # сообщение наше за место пробелов дефисы
 
     # Сохраняем время и запоминаем экземпляры для создания связи
@@ -72,6 +80,10 @@ def query_analyzer_every_week(query, uid):
     """
     Тест пн,ср,пт 10:30 Это-задание,-повторяем-,каждую-неделю-в-10:30
     """
+    response_filter = filter_every_week(query)
+    if response_filter:
+        return response_filter
+
     message = (' ').join(query[3].split('-')) # сообщение наше за место пробелов дефисы
     ScheduleEveryWeek(uid=uid, name=query[0], week_day=query[1], message=message,
                       time=query[2], date=datetime.now().date() - timedelta(days=1)).save()
@@ -83,6 +95,10 @@ def query_analyzer_every_month(query, uid):
     """
     Месяц 1,5,8,26 9:30 Напоминаем-о-молочке
     """
+    response_filter = filter_every_month(query)
+    if response_filter:
+        return response_filter
+
     message = (' ').join(query[3].split('-')) # сообщение наше за место пробелов дефисы
     ScheduleEveryMonth(uid=uid, name=query[0], days=query[1], message=message,
                        time=query[2], date=datetime.now().date() - timedelta(days=1)).save()
@@ -92,8 +108,12 @@ def query_analyzer_every_month(query, uid):
 
 def query_analyzer_every_year(query, uid):
     """
-    Год 24.01 9:00 Повторяем-каждый-год
+    Год 24.01 09:00 Повторяем-каждый-год
     """
+    response_filter = filter_every_year(query)
+    if response_filter:
+        return response_filter
+
     message = (' ').join(query[3].split('-')) # сообщение наше за место пробелов дефисы
     ScheduleEveryYear(uid=uid, name=query[0], day=query[1],
                       message=message, time=query[2], date=datetime.now().date() - timedelta(days=1)).save()
@@ -102,6 +122,10 @@ def query_analyzer_every_year(query, uid):
 
 
 def query_analyzer_day(query, uid):
+
+    response_filter = filter_day(query)
+    if response_filter:
+        return response_filter
 
     message = (' ').join(query[2].split('-')) # сообщение наше за место пробелов дефисы
     ScheduleDay(uid=uid, name=query[0], day=query[1], message=message).save()
